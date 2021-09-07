71562aa27770dcbae81981e832ec3a36c93e97a8
Hitendra10 <you@example.com>
2021-09-07 16:57:30 +0530

    added busy state of elevator

--- src/Function/elevatorNum.js
+++ src/Function/elevatorNum.js
@@ -3,16 +3,20 @@ const eleBusy = [];
 //gets the floor top edge distance and finds the elevator closer to it
 function ElevatorNum(floorTopEdge, elevatorArray) {
 
-  elevatorArray.filter((item) => {
-    return console.log(item !== eleBusy[item]);
-  });
-
-  let busy = false;
+  const eleNotBusy = [];
   const scrolledDistance = document.documentElement.scrollTop;
   const elevatorArrayDist = [];
 
+  elevatorArray.map((e) => {
+    if (!eleBusy.includes(e)) {
+      return eleNotBusy.push(e);
+    }
+  });
+
+if(eleNotBusy.length!==0){
+  //
   //finds distance of elevator from the top of the webpage and appends to an array
-  elevatorArray.map((w) => {
+  eleNotBusy.map((w) => {
     const ele = document.getElementById(w);
     const elePos = Math.round(ele.getBoundingClientRect().y + scrolledDistance);
     const dist = Math.round(Math.abs(floorTopEdge - elePos));
@@ -23,9 +27,7 @@ function ElevatorNum(floorTopEdge, elevatorArray) {
   //finds minimum distance from the array
   const min = Math.min(...elevatorArrayDist);
   const indx = elevatorArrayDist.indexOf(min);
-  //targets the elevator with least distance from the array
-  const eleElement = document.getElementById(elevatorArray[indx]);
-
+  const eleElement = document.getElementById(eleNotBusy[indx]);
 
   function Move() {
     //changes the css of the targeted elevator (speed and top distance)
@@ -34,38 +36,26 @@ function ElevatorNum(floorTopEdge, elevatorArray) {
     //for 2sec per floor speed devide by 75
     //for 1sec per floor speed devide by 150
     eleSent.style.top = `${-20 + floorTopEdge}px`;
-
-    eleBusy.push(eleElement.id);
+    if (!eleBusy.includes(eleElement.id)) {
+      eleBusy.push(eleElement.id);
+    }
   }
 
   //
   //assign the call of user to an idle elevator
   //
-
-  elevatorArrayDist.map((i) => {
-    if (eleElement.id === eleBusy[i]) {
-      return (busy = true);
-    } else {
-      return (busy = false);
-    }
-  });
-
   //remove elevator ID from liftBusy[]
   function removeBusy() {
     eleBusy.splice(0, 1);
-    //console.log(elevatorArray);
   }
   setTimeout(() => {
     removeBusy();
-    console.log("removed from busy " + eleBusy);
-  }, min * 20);
-  console.log("added to busy " + eleBusy);
+  }, (min / 75) * 1000);
 
-  if (busy === true) {
-    alert("Busy");
-  } else {
-    return Move();
-  }
+  Move();
+}else{
+  alert("Elevators are busy please wait")
+}
 }
 
 export default ElevatorNum;
